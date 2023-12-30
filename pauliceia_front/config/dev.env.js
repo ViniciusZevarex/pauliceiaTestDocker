25dc92f449d3472be1a624c55c758474ec27d301
matmak-usp <mateus.caetano@usp.br>
2023-12-30 17:14:05 -0300

    Ajustes de url front e inserção vgiws docker

--- pauliceia_front/config/dev.env.js
+++ pauliceia_front/config/dev.env.js
@@ -5,10 +5,10 @@ const prodEnv = require("./prod.env");
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   // urlVGI: '"http://localhost:3001"',
-  urlVGI: '"https://pauliceia.unifesp.br/api/vgi"',
+  urlVGI: '"http://localhost:8888"',
   // urlGeoserverPauliceia: '"http://localhost:9021/geoserver/pauliceia"',
-  urlGeoserverPauliceia: '"http://geoserver:8080/geoserver/pauliceia"',
-  urlGeoserveOther: '"http://geoserver:8080/geoserver/other"',
+  urlGeoserverPauliceia: '"http://localhost:8001/geoserver/pauliceia"',
+  urlGeoserveOther: '"http://localhost:8001/geoserver/other"',
   urlGeocoding: '"http://localhost:3000/api/geocoding"',
   keyCripto: '"keytest"',
 });
